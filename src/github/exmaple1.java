4db485145b80c8627bec2c9301b69be6bf2c1905
Saidatta Sawant <32416803+saidatta97@users.noreply.github.com>
2022-10-19 13:28:47 +0530

    [ankit]created new example1 class

---
+++ src/github/exmaple1.java
@@ -0,0 +1,14 @@
+package github;
+
+public class exmaple1 
+{
+public static void main(String[] args) 
+{
+	System.out.println("code send to the github");
+	
+	System.out.println("my name is ankit");
+	
+	System.out.println("programme is successfully run");
+	
+}
+}
